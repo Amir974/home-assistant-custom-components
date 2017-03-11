bdd71723df902a566b709d51b29148a1575e7360
Amir Hanan <amir974@gmail.com>
2017-03-11 23:48:34 +0200

    Fixing count & percent
    example of how to use in scripts added to comments

--- custom_components/notify/pushoverglances.py
+++ custom_components/notify/pushoverglances.py
@@ -1,11 +1,62 @@
 """
 Pushover platform for notify via glances - small notifications
 
+# Message only
+    - service: notify.pushoverglances
+      data:
+        message: "glances1"
+#
+# Count only
+    - service: notify.pushoverglances
+      data:
+        message: "False"
+        data:
+          count: "2"
+#
+# Percent only
+    - service: notify.pushoverglances
+      data:
+        message: "False"
+        data:
+          percent: "10"
+#
+# Percent & Title
+    - service: notify.pushoverglances
+      data:
+        message: "False"
+        title: "glances4"
+        data:
+          percent: "90"
+#
+# Message & percent
+    - service: notify.pushoverglances
+      data:
+        message: "glances5"
+        data:
+          percent: "90"
+#
+# Message & percent
+    - service: notify.pushoverglances
+      data:
+        message: "glances6"
+        data:
+          percent: 90
+#
+# Message & count
+    - service: notify.pushoverglances
+      data:
+        message: "glances7"
+        data:
+          count: "7"
+#
+
 for apple iWatch for example!
 """
 import logging
 import http.client, urllib
 import voluptuous as vol
+import time
+import datetime
 
 from homeassistant.components.notify import (
     ATTR_TITLE, ATTR_TITLE_DEFAULT, ATTR_TARGET, ATTR_DATA,PLATFORM_SCHEMA, BaseNotificationService)
@@ -17,6 +68,12 @@ REQUIREMENTS = ['python-pushover==0.2']
 _LOGGER = logging.getLogger(__name__)
 
 CONF_USER_KEY = 'user_key'
+
+# Text to notify user of
+ATTR_MESSAGE = 'message'
+# Title of notification
+ATTR_TITLE = 'title'
+
 # Count attribute is optional in data
 ATTR_COUNT = 'count'
 ATTR_PERCENT = 'percent'
@@ -35,8 +92,7 @@ def get_service(hass, config,discovery_info):
         return PushoverGlanceService(config[CONF_USER_KEY],
                                            config[CONF_API_KEY])
     except InitError:
-        _LOGGER.error(
-            'Wrong API key supplied pushover glances. Get it at https://pushover.net')
+        _LOGGER.error('Wrong API key supplied pushover glances. Get it at https://pushover.net')
         return None
 
 
@@ -54,7 +110,10 @@ class PushoverGlanceService(BaseNotificationService):
     def send_message(self, message='', **kwargs):
         """Send a message to a user."""
         from pushover import RequestError
-
+        ts = time.time()
+        #rTimefull = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+        rTimefull = datetime.datetime.fromtimestamp(ts).strftime('%H:%M')
+		
         # Make a copy and use empty dict if necessary
         data = dict(kwargs.get(ATTR_DATA) or {})
         # Get message title for parssing
@@ -65,10 +124,15 @@ class PushoverGlanceService(BaseNotificationService):
         conn = http.client.HTTPSConnection("api.pushover.net:443")
 
         # Notification can hold Text / Numeric (count) / both
+        gotTitle = False
         gotText = False
         gotCount = False
         gotPercent = False
 
+#        s = "-";
+#        seq = (rTimefull,message); # This is sequence of strings.
+#        timemsg = s.join( seq )
+		
         # If Message ="False" it will not be sent to API
         if message != "False":
             gotText = True
@@ -76,9 +140,10 @@ class PushoverGlanceService(BaseNotificationService):
         # Parse the title & message into glance notification 
         #(if no title is added only message will be sent)
         if myTitle:
-            myText = '{} {}'.format(myTitle, message)
-        else:
-            myText = message
+            gotTitle = True
+#            myText = '{} {}'.format(myTitle, timemsg)
+#        else:
+#            myText = timemsg
 
         # Numeric (count) attribute is optional - check if we got it
         if data is not None and ATTR_COUNT in data:
@@ -88,35 +153,32 @@ class PushoverGlanceService(BaseNotificationService):
         if data is not None and ATTR_PERCENT in data:
             myPercent = data.get(ATTR_PERCENT, None)
             gotPercent = True
-
-        if gotCount and gotPercent:
-            # Message & Numeric (count) notification will be sent
-            _LOGGER.debug("Pushover glances pushing Percent =%s", myPercent)
-            conn.request("POST", "/1/glances.json", urllib.parse.urlencode({ "token": self._api_token, "user":self._user_key, "title": myTitle , "percent": myPercent}), { "Content-type": "application/x-www-form-urlencoded" })
-            try:
-               conn.getresponse()
-            except ValueError as val_err:
-               _LOGGER.error(str(val_err))
-            except RequestError:
-               _LOGGER.exception('Could not send pushover glances notification')
-
-        if gotCount and gotText:
-            # Message & Numeric (count) notification will be sent
-            _LOGGER.debug("Pushover glances pushing Count & Text =%s", '{} {}'.format(myCount, myText))
-            conn.request("POST", "/1/glances.json", urllib.parse.urlencode({ "token": self._api_token, "user":self._user_key, "text": myText, "count": myCount}), { "Content-type": "application/x-www-form-urlencoded" })
-            # Only Message notification will be sent
-        elif gotText:
-            _LOGGER.debug("Pushover glances pushing Text =%s", myText)
-            conn.request("POST", "/1/glances.json", urllib.parse.urlencode({ "token": self._api_token, "user":self._user_key, "text": myText}), { "Content-type": "application/x-www-form-urlencoded" })
-        elif gotCount:
-            # Only Numeric (count) notification will be sent
-            _LOGGER.debug("Pushover glances pushing Count =%s", myCount)
-            conn.request("POST", "/1/glances.json", urllib.parse.urlencode({ "token": self._api_token, "user":self._user_key, "count": myCount}), { "Content-type": "application/x-www-form-urlencoded" })			
-
-            # Percent and some of the other attributes in glances API 
-            # do not seem to effect iWatch complication so not handled for now
 			
+        _LOGGER.debug("Glances gotTitle = %s, gotText = %s, gotCount = %s, gotPercent = %s", str(gotTitle), str(gotText), str(gotCount), str(gotPercent))
+		
+        urlbuilder = { "token": self._api_token}
+        _LOGGER.debug("Building = %s", str(urlbuilder))
+        urlbuilder ["user"] = self._user_key
+        _LOGGER.debug("Building = %s", str(urlbuilder))
+        if gotTitle: 
+          urlbuilder ["title"] = myTitle
+        else:
+          urlbuilder ["title"] = str(rTimefull)
+        _LOGGER.debug("Building = %s", str(urlbuilder))
+        if gotText:
+          urlbuilder ["text"] = str(message)
+          _LOGGER.debug("Building = %s", str(urlbuilder))
+          _LOGGER.debug("message = %s", str(message))
+        if gotCount:
+          urlbuilder["count"] = int(myCount)
+          _LOGGER.debug("Building = %s", str(urlbuilder))
+        if gotPercent:
+          urlbuilder["percent"] = int(myPercent)
+          _LOGGER.debug("Building = %s", str(urlbuilder))
+		
+        conn.request("POST", "/1/glances.json", urllib.parse.urlencode(urlbuilder), { "Content-type": "application/x-www-form-urlencoded" })
         try:
+           _LOGGER.debug("got it!")
            conn.getresponse()
         except ValueError as val_err:
            _LOGGER.error(str(val_err))
