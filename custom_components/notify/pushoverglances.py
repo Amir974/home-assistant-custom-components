a7d2946444b1d62e5e22f093bc2d242ee47885ce
Amir Hanan <amir974@gmail.com>
2017-03-14 12:30:37 +0200

    Do-over... big fix and changes
    Optimizing code, adding subtext option and us usage examples in the
    comment ontop

--- custom_components/notify/pushoverglances.py
+++ custom_components/notify/pushoverglances.py
@@ -1,56 +1,32 @@
 """
 Pushover platform for notify via glances - small notifications
-
-# Message only
-    - service: notify.pushoverglances
-      data:
-        message: "glances1"
-#
-# Count only
-    - service: notify.pushoverglances
-      data:
-        message: "False"
-        data:
-          count: "2"
-#
-# Percent only
-    - service: notify.pushoverglances
-      data:
-        message: "False"
-        data:
-          percent: "10"
-#
-# Percent & Title
-    - service: notify.pushoverglances
-      data:
-        message: "False"
-        title: "glances4"
-        data:
-          percent: "90"
-#
-# Message & percent
-    - service: notify.pushoverglances
-      data:
-        message: "glances5"
-        data:
-          percent: "90"
-#
-# Message & percent
-    - service: notify.pushoverglances
-      data:
-        message: "glances6"
-        data:
-          percent: 90
-#
-# Message & count
-    - service: notify.pushoverglances
-      data:
-        message: "glances7"
-        data:
-          count: "7"
-#
-
 for apple iWatch for example!
+
+#
+glances1:
+  sequence:
+    - service: notify.pushoverglances
+      data_template:
+        message: "Rst msg"
+        title: "Rst ttl"
+        data:
+          count: "100"
+          precent: "100"
+          subtext: "Rst subtxt"
+#
+glances2:
+  sequence:
+    - service: notify.pushoverglances
+      data_template:
+        message: "{{states('sensor.amir_work_to_home')}}msg"
+        title: "{{states('sensor.amir_work_to_home')}} ttl"
+        data:
+          count: "{{states('sensor.amir_work_to_home')}}"
+          precent: "{{(states('sensor.amir_work_to_home' ) | int) * 2}}"
+          subtext: "{{((states('sensor.amir_work_to_home' ) | int) * 3)}}"
+#
+
+
 """
 import logging
 import http.client, urllib
@@ -59,31 +35,27 @@ import time
 import datetime
 
 from homeassistant.components.notify import (
-    ATTR_TITLE, ATTR_TITLE_DEFAULT, ATTR_TARGET, ATTR_DATA,PLATFORM_SCHEMA, BaseNotificationService)
+    ATTR_TITLE, ATTR_TARGET, ATTR_DATA,PLATFORM_SCHEMA, BaseNotificationService)
 from homeassistant.const import CONF_API_KEY
 import homeassistant.helpers.config_validation as cv
 
-#Keeping structure of original pushover home-assistant implementation for validation
 REQUIREMENTS = ['python-pushover==0.2']
 _LOGGER = logging.getLogger(__name__)
 
 CONF_USER_KEY = 'user_key'
 
-# Text to notify user of
 ATTR_MESSAGE = 'message'
-# Title of notification
 ATTR_TITLE = 'title'
 
-# Count attribute is optional in data
 ATTR_COUNT = 'count'
 ATTR_PERCENT = 'percent'
+ATTR_SUBTEXT = 'subtext'
 
 PLATFORM_SCHEMA = cv.PLATFORM_SCHEMA.extend({
     vol.Required(CONF_USER_KEY): cv.string,
     vol.Required(CONF_API_KEY): cv.string,
 })
 
-# pylint: disable=unused-variable
 def get_service(hass, config,discovery_info):
     """Get the Pushover notification service (Verify the keys)"""
     from pushover import InitError
@@ -111,71 +83,39 @@ class PushoverGlanceService(BaseNotificationService):
         """Send a message to a user."""
         from pushover import RequestError
         ts = time.time()
-        #rTimefull = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
         rTimefull = datetime.datetime.fromtimestamp(ts).strftime('%H:%M')
 		
-        # Make a copy and use empty dict if necessary
-        data = dict(kwargs.get(ATTR_DATA) or {})
-        # Get message title for parssing
-        myTitle = kwargs.get(ATTR_TITLE)
-		
-        data['title'] = kwargs.get(ATTR_TITLE, ATTR_TITLE_DEFAULT)
+        data = kwargs.get(ATTR_DATA)
+        myTitle = kwargs.get(ATTR_TITLE, rTimefull)
         # Set connection for API
         conn = http.client.HTTPSConnection("api.pushover.net:443")
-
-        # Notification can hold Text / Numeric (count) / both
-        gotTitle = False
-        gotText = False
-        gotCount = False
-        gotPercent = False
-
-#        s = "-";
-#        seq = (rTimefull,message); # This is sequence of strings.
-#        timemsg = s.join( seq )
-		
-        # If Message ="False" it will not be sent to API
-        if message != "False":
-            gotText = True
-		
-        # Parse the title & message into glance notification 
-        #(if no title is added only message will be sent)
-        if myTitle:
-            gotTitle = True
-#            myText = '{} {}'.format(myTitle, timemsg)
-#        else:
-#            myText = timemsg
-
-        # Numeric (count) attribute is optional - check if we got it
-        if data is not None and ATTR_COUNT in data:
-            myCount = data.get(ATTR_COUNT, None)
-            gotCount = True
-        
-        if data is not None and ATTR_PERCENT in data:
-            myPercent = data.get(ATTR_PERCENT, None)
-            gotPercent = True
-			
-        _LOGGER.debug("Glances gotTitle = %s, gotText = %s, gotCount = %s, gotPercent = %s", str(gotTitle), str(gotText), str(gotCount), str(gotPercent))
 		
         urlbuilder = { "token": self._api_token}
-        _LOGGER.debug("Building = %s", str(urlbuilder))
         urlbuilder ["user"] = self._user_key
-        _LOGGER.debug("Building = %s", str(urlbuilder))
-        if gotTitle: 
-          urlbuilder ["title"] = myTitle
-        else:
-          urlbuilder ["title"] = str(rTimefull)
-        _LOGGER.debug("Building = %s", str(urlbuilder))
-        if gotText:
-          urlbuilder ["text"] = str(message)
-          _LOGGER.debug("Building = %s", str(urlbuilder))
-          _LOGGER.debug("message = %s", str(message))
-        if gotCount:
-          urlbuilder["count"] = int(myCount)
-          _LOGGER.debug("Building = %s", str(urlbuilder))
-        if gotPercent:
-          urlbuilder["percent"] = int(myPercent)
-          _LOGGER.debug("Building = %s", str(urlbuilder))
 		
+        if myTitle:
+          urlbuilder ["title"] = myTitle
+
+        if message != "False":
+          urlbuilder ["text"] = str(message)
+          _LOGGER.debug("message = %s", str(message))
+		  
+        if data is not None and ATTR_COUNT in data:
+          myCount = data.get(ATTR_COUNT, None)
+          _LOGGER.debug("myCount = %s", str(myCount))
+          urlbuilder["count"] = str(myCount).strip("''")
+
+        if data is not None and ATTR_PERCENT in data:
+          myPercent = data.get(ATTR_PERCENT, None)
+          _LOGGER.debug("myPercent = %s", str(myPercent))
+          urlbuilder["percent"] = str(myPercent).strip("''")
+		  
+        if data is not None and ATTR_SUBTEXT in data:
+          mySubText = data.get(ATTR_SUBTEXT, None)
+          _LOGGER.debug("mySubText = %s", str(mySubText))
+          urlbuilder["subtext"] = str(mySubText).strip("''")
+          
+        _LOGGER.debug("Building = %s", str(urlbuilder))
         conn.request("POST", "/1/glances.json", urllib.parse.urlencode(urlbuilder), { "Content-type": "application/x-www-form-urlencoded" })
         try:
            _LOGGER.debug("got it!")
